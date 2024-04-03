fdff318608b742756cf62a808fdff485a301eadd
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-04-03 12:44:37 +0200

    choose methods file

--- R/methodsTable.R
+++ R/methodsTable.R
@@ -1,13 +1,27 @@
 #' @export
-getMethodTableHyper <- function(dbPath) {
-  methodsTableFile <- file.path(dbPath, "_hyper", "methods.csv")
-  methods <- readr::read_csv(
-    methodsTableFile,
-    col_types = readr::cols(
-      model = readr::col_character(),
-      obs = readr::col_character(),
-      method = readr::col_character(),
-      timeInMinutes = readr::col_integer()
-    ))
-  return(methods)
+getMethodTableNames <- function(dbPath) {
+  methodTableFiles <- list.files(
+    file.path(dbPath, "_hyper"),
+    pattern = "^methods.*\\.csv$")
+  return(methodTableFiles)
+}
+
+
+#' @export
+getMethodTable <- function(dbPath, methodTableNames) {
+  methodsTableFiles <- file.path(dbPath, "_hyper", methodTableNames)
+  colTypes <- readr::cols(
+    model = readr::col_character(),
+    obs = readr::col_character(),
+    method = readr::col_character(),
+    timeInMinutes = readr::col_integer()
+  )
+  methodTable <-
+    lapply(
+      methodsTableFiles,
+      readr::read_csv,
+      col_types = colTypes
+    ) |>
+    dplyr::bind_rows()
+  return(methodTable)
 }
