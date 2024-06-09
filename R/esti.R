d080603b9fc05b57ccd9bf7bef52efe7254530b1
Christof Schoetz <math@christof-schoetz.de>
2024-06-09 22:21:06 +0200

    fix

--- R/esti.R
+++ R/esti.R
@@ -22,7 +22,7 @@ getObservationNrs <- function(dbPath, model, obsNrFilter = NULL) {
 
 #' @export
 getMethodFile <- function(dbPath, method) {
-  file.path(hyperDir(), paste0(method, ".json"))
+  file.path(hyperDir(dbPath), paste0(method, ".json"))
 }
 
 #' @export
