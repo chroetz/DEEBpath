2b27216e5d9c27aa7932c535476ea00e32dda82a
Christof Schoetz <math@christof-schoetz.de>
2024-06-13 12:41:27 +0200

    fix

--- R/DEEBpath-package.R
+++ R/DEEBpath-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 #' @import stringr
 #' @importFrom rlang .data .env
-#' @importFrom dplyr tibble as_tibble select filter mutate left_join full_join anti_join join_by bind_rows bind_cols distinct
+#' @importFrom dplyr tibble as_tibble select filter mutate left_join full_join anti_join join_by bind_rows bind_cols distinct distinct
 #' @importFrom tidyr drop_na
 "_PACKAGE"
 
