5dd8afee4cece3e69d289a7e8b542abc28b427af
Christof Schötz <math@christof-schoetz.de>
2024-06-21 16:07:41 +0200

    get methods

--- R/esti.R
+++ R/esti.R
@@ -9,6 +9,27 @@ getModels <- function(dbPath, modelPattern=NULL) {
   return(models)
 }
 
+
+#' @export
+getMethods <- function(dbPath, models = NULL, methodPattern=NULL) {
+  if (is.null(models)) models <- getModels(dbPath)
+  tbl <-
+    lapply(models, \(model) {
+      paths <- getPaths(dbPath, model)
+      methodDirPaths <- list.dirs(paths$esti, recursive=FALSE, full.names=TRUE)
+      methods <- basename(methodDirPaths)
+      if (!is.null(methodPattern)) {
+        sel <- str_detect(methods, methodPattern)
+        methods <- methods[sel]
+        methodDirPaths <- methodDirPaths[sel]
+      }
+      tibble(model = model, method = methods, path = methodDirPaths)
+    }) |>
+    bind_rows()
+  return(tbl)
+}
+
+
 #' @export
 getObservationNrs <- function(dbPath, model, obsNrFilter = NULL) {
   paths <- getPaths(dbPath, model)
